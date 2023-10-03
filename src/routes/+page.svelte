ff7871dfa9e63c97f15ae58860ca3347f7cb13b2
Kai-Uwe Chromik <kai.chromik@online.de>
2023-10-03 18:19:25 +0200

    bootstrap included

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,22 @@
 <script>
     export let data;
 </script>
-<h1>Zitat des Tages:</h1>
-<quote>{data.data[0].q}</quote>
-<small>{data.data[0].a}</small>
+<div class="position-relative test">
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <h1>Zitat des Tages:</h1>
+        <br>
+        <figure>
+            <blockquote class="blockquote">
+            <p>{data.data[0].q}</p>
+             </blockquote>
+            <figcaption class="blockquote-footer">
+            {data.data[0].a} <cite title="Source">in Chromik des Jahrhunderts</cite>
+            </figcaption>
+        </figure>
+    </div>
+</div>
+<style>
+    .test {
+        height: 100vh;
+    }
+</style>
